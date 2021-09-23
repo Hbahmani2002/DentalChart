d8fb8f32347fa8836bf354405a3d74de49c9d07a
Protek <Protek@DESKTOP-TLIQ89T>
2021-09-23 11:57:58 +0300

    Hassan 2021-09-23-01

--- src/main/java/shadow/dental_chart/App.java
+++ src/main/java/shadow/dental_chart/App.java
@@ -33,19 +33,19 @@ import javafx.scene.control.Button;
 public class App extends Application {
 
     private static Scene scene;
-    public static String ad="x";
-    public static String soyad="x";
-    public static String tc="x";
-    public static String protokol="x";
-     public static String port="x";
-    public static String link="x";
-     public static String kullid="x";
+    public static String ad = "x";
+    public static String soyad = "x";
+    public static String tc = "x";
+    public static String protokol = "x";
+    public static String port = "x";
+    public static String link = "x";
+    public static String kullid = "x";
 
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML(DentalChartController.class.getSimpleName()), 1650, 768);
         stage.setScene(scene);
-       stage.setResizable(false);
+        stage.setResizable(false);
         stage.show();
     }
 
@@ -54,87 +54,89 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String name) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/"+name + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/" + name + ".fxml"));
         /* I had performance and lag issues  using the usuall javafx ScrollPane specially after wrapping the chart content inside a Group
           ScrollPane
                -> Group
                          -> VBox
                                    -> ....chart content
 
-        */
-        MyScrollPane scrollPane=new MyScrollPane();
-        scrollPane.setContent(fxmlLoader.load());        
+         */
+        MyScrollPane scrollPane = new MyScrollPane();
+        scrollPane.setContent(fxmlLoader.load());
         scrollPane.getStylesheets().add(App.class.getResource("/css/chart.css").toExternalForm());
         return scrollPane;
     }
 
     public static void main(String[] args) throws IOException {
-      try {
+        try {
 
-    if (args.length > 0) {
-if(args[0].length()>15) {
-    String gelenstr = args[0].substring(args[0].lastIndexOf("//") + 2);
-    if (containsChar(gelenstr,'/'))
-        gelenstr=gelenstr.substring(0,gelenstr.indexOf('/'));
-  byte[] decodedBytes = Base64.getDecoder().decode(gelenstr);
-String decodedString = new String(decodedBytes);
+            if (args.length > 0) {
+                if (args[0].length() > 15) {
+                    String gelenstr = args[0].substring(args[0].lastIndexOf("//") + 2);
+                    if (containsChar(gelenstr, '/')) {
+                        gelenstr = gelenstr.substring(0, gelenstr.indexOf('/'));
+                    }
+                    byte[] decodedBytes = Base64.getDecoder().decode(gelenstr);
+                    String decodedString = new String(decodedBytes);
 
-    
-   
-    String[] parts = decodedString.split(" ");
-    protokol=parts[0];
-    link=parts[1];
-    port=parts[2];
-    kullid=parts[3];
-    ad = parts[4];
-    soyad = parts[5];
-    tc = parts[6];
-}
+                    String[] parts = decodedString.split(" ");
+                    protokol = parts[0];
+                    link = parts[1];
+                    port = parts[2];
+                    kullid = parts[3];
+                    ad = parts[4];
+                    soyad = parts[5];
+                    tc = parts[6];
+                }
+            }
+
+            launch();
+        } catch (Exception ex) {
+            showAlert(ex.getMessage());
+
+            FileWriter fileWriter = new FileWriter("c:\\DentalChart\\Error.txt");
+            PrintWriter printWriter = new PrintWriter(fileWriter);
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+            Date date = new Date(System.currentTimeMillis());
+
+            printWriter.print(date + " : " + ex.getMessage());
+            printWriter.close();
+            // System.exit(0);
+        }
     }
 
-    launch();
-}
-catch (Exception ex)
-{
-    showAlert(ex.getMessage());
-
-    FileWriter fileWriter = new FileWriter("c:\\DentalChart\\Error.txt");
-    PrintWriter printWriter = new PrintWriter(fileWriter);
-    SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
-Date date = new Date(System.currentTimeMillis());
-
-    printWriter.print(date+" : "+ ex.getMessage());
-    printWriter.close();
-   // System.exit(0);
-}
-    }
     public static void showAlert(String message) {
-    Platform.startup(new Runnable() {
-      public void run() {
-          Alert alert = new Alert(Alert.AlertType.ERROR);
-          alert.setTitle("Hata...");
-          alert.setHeaderText("Bu hata ilk Açılış hatası");
-          alert.setContentText(message);
-          alert.show();
-      }
-    });
+        Platform.startup(new Runnable() {
+            public void run() {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Hata...");
+                alert.setHeaderText("Bu hata ilk Açılış hatası");
+                alert.setContentText(message);
+                
+                alert.show();
+            }
+        });
     }
-      public static void showAlertOut(String message) {
-    Platform.runLater(new Runnable() {
-      public void run() {
-          Alert alert = new Alert(Alert.AlertType.ERROR);
-          alert.setTitle("Hata...");
-          alert.setHeaderText("Bu hata Servis Bağlantı Hatasi");
-          alert.setContentText(message);
-          alert.showAndWait();
-      }
-    });
-}
+
+    public static void showAlertOut(String message) {
+        Platform.runLater(new Runnable() {
+            public void run() {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Hata...");
+                alert.setHeaderText("Bu hata Servis Bağlantı Hatasi");
+                alert.setContentText(message);
+                alert.showAndWait();
+            }
+        });
+    }
+
     public static boolean containsChar(String s, char search) {
-    if (s.length() == 0)
-        return false;
-    else
-        return s.charAt(0) == search || containsChar(s.substring(1), search);
-}
-  
+        if (s.length() == 0) {
+            return false;
+        } else {
+            return s.charAt(0) == search || containsChar(s.substring(1), search);
+        }
+    }
+
 }
